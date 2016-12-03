3eb6b231c210793a75ff7edf08745257a8a208fe
Alameyo <pawelsc@gmail.com>
2016-12-03 12:48:35 +0100

    Project setup

---
+++ android/src/com/alameyo/jpwp/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.alameyo.jpwp;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.alameyo.jpwp.MainClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainClass(), config);
+	}
+}
